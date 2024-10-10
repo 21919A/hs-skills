2775384e86e2a2fcbafe47d6cb5360ad5c19773b
hatatat-dev <hatatat@glowstonemania.com>
2024-10-09 20:14:43 -0700

    [log] log autonomous_begin before other steps
    also remove old commented-out code

--- src/main.py
+++ src/main.py
@@ -13,21 +13,21 @@ def driver_function():
     """Function for the driver part of a competition match"""
 
     log(("Competition", "competition"), "driver_begin")
+
+    # General event handling is initialized outside of this function by init_event_handling()
+
     controller.buttonX.pressed(lambda: pid_driver.drive(1000))
     controller.buttonY.pressed(lambda: pid_driver.drive(-1000))
-    controller.buttonLeft.pressed(lambda: pid_turner.turn(-90,FRAME_HEADING_RELATIVE))
+    controller.buttonLeft.pressed(lambda: pid_turner.turn(-90, FRAME_HEADING_RELATIVE))
     controller.buttonRight.pressed(lambda: pid_turner.turn(90, FRAME_HEADING_RELATIVE))
 
-    # Add driver logic here
-    # Note that event handling is initialized outside of this function by init_event_handling()
-
     log(("Competition", "competition"), "driver_end")
-
 
 
 def autonomous_function():
     """Function for the autonomous part of a competition match"""
 
+    log(("Competition", "competition"), "autonomous_begin")
 
     # Reset odometry to the starting autonomous position
     odometry.reset(PositionWithHeading(-1500, 600, 270))
@@ -35,12 +35,8 @@ def autonomous_function():
     # # Then try resetting it to GPS if GPS sensor is installed and reports high quality
     reset_odometry_to_gps()
 
-    log(("Competition", "competition"), "autonomous_begin")
-
     intake_1st_stage.set_velocity(450, RPM)
     intake_2nd_stage.set_velocity(450, RPM)
-
-
 
     wait(100, MSEC)
 
@@ -54,11 +50,10 @@ def autonomous_function():
     intake_1st_stage.spin(REVERSE)
     intake_2nd_stage.spin(FORWARD)
 
-  
     pid_turner.turn(90, FRAME_ABSOLUTE)
 
     pid_driver.drive(600)
-    
+
     wait(250, MSEC)
     reset_odometry_to_gps()
     pid_turner.turn(0, FRAME_ABSOLUTE)
@@ -117,38 +112,6 @@ def autonomous_function():
     intake_2nd_stage.spin(REVERSE)
     clamp.set(False)
     pid_driver.drive(200)
-
-
-
-    # pid_mover.move(Position(1200, -1200), FRAME_ABSOLUTE)
-
-    # sleep(1000, TimeUnits.MSEC)
-
-    # reset_odometry_to_gps()
-
-    # pid_mover.move(Position(-1200, -1200), FRAME_ABSOLUTE)
-
-    # sleep(1000, TimeUnits.MSEC)
-
-    # reset_odometry_to_gps()
-
-    # pid_mover.move(Position(-1200, 1200), FRAME_ABSOLUTE)
-
-    # sleep(1000, TimeUnits.MSEC)
-
-    # reset_odometry_to_gps()
-
-    # pid_mover.move(Position(1200, 1200), FRAME_ABSOLUTE)
-
-    # sleep(1000, TimeUnits.MSEC)
-
-    # reset_odometry_to_gps()
-
-    # pid_aimer.aim(Position(1200, -1200), FRAME_ABSOLUTE)
-
-    # sleep(1000, TimeUnits.MSEC)
-
-    # reset_odometry_to_gps()
 
     log(("Competition", "competition"), "autonomous_end")
 
